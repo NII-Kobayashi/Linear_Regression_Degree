e1968b502af7b67a5c02987cc3415f847436b3e9
Niharika <niharika.singhal@rwth-aachen.de>
2018-06-05 15:41:25 +0900

    some changes

--- estimate.py
+++ estimate.py
@@ -34,6 +34,7 @@ def parameter_estimation_lr_n(follower_orig, total_follower_t, no_events, event_
     ])
 
     y1 = sum(event_pred)
+    y1_len = len(sum(event_pred))
     y2 = sum([(no_events[i] * event_pred[i]) for i in range(len(event_pred))])
     y3 = sum([(total_follower_t[i] * event_pred[i]) for i in range(len(event_pred))])
     y4 = sum([(follower_orig[i] * event_pred[i]) for i in range(len(event_pred))])
@@ -47,7 +48,8 @@ def parameter_estimation_lr_n(follower_orig, total_follower_t, no_events, event_
         return theta, sigma_sq
 
     else:
-        theta = [linalg.solve(a, (np.matrix([y1, y2, y3, y4])).transpose())]
+        theta = [linalg.solve(a, (np.matrix([y1[i], y2[i], y3[i], y4[i]])).transpose())
+                 for i in range(y1_len)]
         sigma_sq_list = []
         for j in range(len(theta)):
             val = [(event_pred[i][j] - theta[j][0] - (theta[j][1] * no_events[i]) -
