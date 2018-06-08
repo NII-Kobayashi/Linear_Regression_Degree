6f288cc2cf687601fce86ca02890865cd5ef44b7
Niharika <niharika.singhal@rwth-aachen.de>
2018-06-08 11:57:38 +0900

    changed in code

--- example.py
+++ example.py
@@ -29,7 +29,7 @@ filename = "Data/training/RT*.txt"
 file_list = sorted(gb.glob(filename), key=numerical_sort)  # for files having tweet more than 20000 (RT186. RT1439)
 
 parameters_value = [no_of_events_followers(file_list[i], T_OBS, T_PRED, 3600) for i in range(len(file_list))]
-
+parameters_value = list(filter(None.__ne__, parameters_value))  # checking for none value
 follower_orig_log = np.asarray([(parameters_value[i][0]) for i in range(len(parameters_value))])
 total_follower_t_log = np.asarray([(parameters_value[i][1]) for i in range(len(parameters_value))])
 no_events_log = np.asarray([(parameters_value[i][2]) for i in range(len(parameters_value))])
@@ -43,7 +43,15 @@ file_name_test = "Data/test/RT*.txt"  # path to the files used for prediction
 file_list_test = sorted(gb.glob(file_name_test), key=numerical_sort)  # for all the training file
 parameters_value_pred = [no_of_events_followers(file_list_test[i], T_OBS, T_PRED, 3600)
                          for i in range(len(file_list_test))]
+parameters_value_pred = list(filter(None.__ne__, parameters_value_pred))  # checking for none value
+follower_orig_log_test = np.asarray([(parameters_value_pred[i][0]) for i in range(len(parameters_value_pred))])
+total_follower_t_log_test = np.asarray([(parameters_value_pred[i][1]) for i in range(len(parameters_value_pred))])
+no_events_log_test = np.asarray([(parameters_value_pred[i][2]) for i in range(len(parameters_value_pred))])
+event_pred_log_true = np.asarray([(parameters_value_pred[i][3]) for i in range(len(parameters_value_pred))])
 
-nfile_prediction_result = [prediction_lr_n(parameters_estimated, no_events_log[i], total_follower_t_log[i],
-                                           follower_orig_log[i]) for i in range(len(no_events_log))]
+nfile_prediction_result = [prediction_lr_n(parameters_estimated, no_events_log_test[i], total_follower_t_log_test[i],
+                                           follower_orig_log_test[i]) for i in range(len(no_events_log_test))]
 print(nfile_prediction_result)
+
+
+
